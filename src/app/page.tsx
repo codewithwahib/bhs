066e62bdd178b034001ea1edf4229c17a60aad9e
codewithwahib <wahibjaffer043@gmail.com>
2025-01-10 18:53:14 +0500

    dsds

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Hero from './components/hero';
+import { client } from '@/sanity/lib/client'
+
+ const  Homepage = async() => {
+     const query = `*[_type == 'blog']  | order(_updatedAt asc){
+  Title,Paragraph,image,
+   "slug":slug.current
+}`
+
+const data:Blog[] = await client.fetch(query)
+
+
+  return (
+    <div className='' >
+       
+       {  data.map((data:Blog)=>(
+        <Hero data={data} key={data.slug}/>
+       ))
+        
+        }
+        
+
+    </div>
+  )
+}
+
+export default Homepage
